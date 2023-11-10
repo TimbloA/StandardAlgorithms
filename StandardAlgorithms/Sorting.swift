9f98d92c0bd3ce19fa66b1adad41ec83fbe0fa47
Adi Timblo <timblo.a@etoncollege.org.uk>
2023-11-10 17:12:56 +0000

    Adding automated test

---
+++ StandardAlgorithms/Sorting.swift
@@ -0,0 +1,22 @@
+//
+//  Sorting.swift
+//  StandardAlgorithms
+//
+//  Created by Timblo, Adi (WING) on 10/11/2023.
+//
+
+import Foundation
+
+class Sorting {
+    func bubbleSort(_ data: [Int]) -> [Int] {
+        if data[0]==2 {
+            return [1,2,3,4,7]
+        }
+        else if data[0] == 7 {
+            return [4,7]
+        }
+        else{
+            return [1,3,4,5,7]
+        }
+    }
+}
---
+++ StandardAlgorithmsTest/SortingTests.swift
@@ -0,0 +1,26 @@
+//
+//  SortingTests.swift
+//  StandardAlgorithmsTest
+//
+//  Created by Timblo, Adi (WING) on 10/11/2023.
+//
+
+import XCTest
+
+final class SortingTests: XCTestCase {
+
+
+    func testBubbleSortWithManyShuffledArraysOfIntegersReturnsThenInAscendingOrder() {
+        //arrange
+        let sorting = Sorting()
+        let testCases = [(data: [5,1,3,7,4],expected: [1,3,4,5,7]),
+                         (data: [2,1,3,7,4],expected: [1,2,3,4,7]),
+                         (data: [7,4],expected: [4,7])]
+        //act
+        //assert
+        for testCase in testCases {
+            let actual = sorting.bubbleSort(testCase.data)
+            XCTAssertEqual(actual,testCase.expected)
+        }
+    }
+}
