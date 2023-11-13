31b51c5f86a3d3adc7f61ee153c5add798329b73
Adi Timblo <timblo.a@etoncollege.org.uk>
2023-11-13 20:47:10 +0000

    Merge Sort EW. Added tests and merge sort function

--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -19,7 +19,52 @@ class Sorting {
             return [1,3,4,5,7]
         }
     }
+    
     func mergeSort(_ data: [Int]) -> [Int] {
-        return data
+        
+        var data1 = data
+        
+        if data1.count > 1 {
+            let midPoint: Int = (data1.count)/2
+            
+            let array1 = mergeSort(Array(data1[0..<midPoint]))
+            let array2 = mergeSort(Array(data1[midPoint..<data.count]))
+            
+            var index1 = 0
+            var index2 = 0
+            var index3 = 0
+            
+            while index1 < array1.count && index2 < array2.count {
+                    if array1[index1] < array2[index2]{
+                data1[index3] = array1[index1]
+                index1 += 1
+                }
+                else{
+                    data1[index3] = array2[index2]
+                    index2 += 1
+                }
+                index3 += 1
+                
+                while index1 < array1.count {
+                    data1[index3] = array1[index1]
+                    index1 += 1
+                    index3 += 1
+                }
+                
+                while index2 < array2.count {
+                    data1[index3] = array2[index2]
+                    index2 += 1
+                    index3 += 1
+                }
+            
+        }
+        
+                    
+            
+            
+            
+        
+        }  
+        return data1
     }
 }
--- StandardAlgorithmsTest/SortingTests.swift
+++ StandardAlgorithmsTest/SortingTests.swift
@@ -27,8 +27,15 @@ final class SortingTests: XCTestCase {
     func testMergeSortWithManyShuffledArraysOfIntegersReturnsThemInAscendingOrder() {
         //arange
         let sorting = Sorting()
+        let testCases = [(data: [5,1,3,7,4],expected: [1,3,4,5,7]),
+                         (data: [2,1,3,7,4],expected: [1,2,3,4,7]),
+                         (data: [7,4],expected: [4,7])]
         //act
         
         //assert
+        for testCase in testCases {
+            let actual = sorting.bubbleSort(testCase.data)
+            XCTAssertEqual(actual,testCase.expected)
+        }
     }
 }
