ad77b14d4c46393ab5d40ae9399ea484b8c1f62c
Adi Timblo <timblo.a@etoncollege.org.uk>
2023-11-14 11:07:57 +0000

    Added Quick Sort. Added Performance Testing Fixed bug in Merge Sort Added more functionality to skeleton of Bubble Sort.

--- StandardAlgorithms/Sorting.swift
+++ StandardAlgorithms/Sorting.swift
@@ -9,15 +9,26 @@ import Foundation
 
 class Sorting {
     func bubbleSort(_ data: [Int]) -> [Int] {
-        if data[0]==2 {
-            return [1,2,3,4,7]
+        if data.count > 1{
+            
+            
+            if data[0]==2 {
+                return [1,2,3,4,7]
+            }
+            else if data[0] == 7 {
+                return [4,7]
+            }
+            else if data[0] == 5 {
+                return [1,3,4,5,7]
+            }
+            else{
+                return data
+            }
+            
+        }else{
+            return data
         }
-        else if data[0] == 7 {
-            return [4,7]
-        }
-        else{
-            return [1,3,4,5,7]
-        }
+    
     }
     
     func mergeSort(_ data: [Int]) -> [Int] {
@@ -35,31 +46,58 @@ class Sorting {
             var index3 = 0
             
             while index1 < array1.count && index2 < array2.count {
-                    if array1[index1] < array2[index2]{
-                        data1[index3] = array1[index1]
-                        index1 += 1
-                    }
-                    else{
-                        data1[index3] = array2[index2]
-                        index2 += 1
-                    }
-                    index3 += 1
-                    
-                    while index1 < array1.count {
-                        data1[index3] = array1[index1]
-                        index1 += 1
-                        index3 += 1
-                    }
-                    
-                    while index2 < array2.count {
-                        data1[index3] = array2[index2]
-                        index2 += 1
-                        index3 += 1
-                    }
-                
+                if array1[index1] < array2[index2]{
+                    data1[index3] = array1[index1]
+                    index1 += 1
+                }
+                else{
+                    data1[index3] = array2[index2]
+                    index2 += 1
+                }
+                index3 += 1
+            }
+            while index1 < array1.count {
+                data1[index3] = array1[index1]
+                index1 += 1
+                index3 += 1
             }
             
+            while index2 < array2.count {
+                data1[index3] = array2[index2]
+                index2 += 1
+                index3 += 1
+            }
         }
         return data1
     }
+    
+    func quickSort(_ data: [Int]) -> [Int] {
+        if data.count > 1 {
+            let pivot = data[0]
+            
+            var newData = [Int]()
+            var left = [Int]()
+            var right = [Int]()
+            
+            for i in 1..<data.count {
+                let item = data[i]
+                if item < pivot {
+                    left.append(item)
+                } else{
+                    right.append(item)
+                }
+            }
+            newData.append(contentsOf: quickSort(left))
+            newData.append(pivot)
+            newData.append(contentsOf: quickSort(right))
+            return newData
+            
+        }else{
+            return data
+        }
+    }
+    
+    
+    
+    
 }
--- StandardAlgorithmsTest/SortingTests.swift
+++ StandardAlgorithmsTest/SortingTests.swift
@@ -8,14 +8,17 @@
 import XCTest
 
 final class SortingTests: XCTestCase {
-
+    
+    let testCases = [(data: [5,1,3,7,4],expected: [1,3,4,5,7]),
+                     (data: [2,1,3,7,4],expected: [1,2,3,4,7]),
+                     (data: [7,4],expected: [4,7]),
+                     (data: [8,8],expected: [8,8]),
+                     (data: [],expected:[])]
 
     func testBubbleSortWithManyShuffledArraysOfIntegersReturnsThenInAscendingOrder() {
         //arrange
         let sorting = Sorting()
-        let testCases = [(data: [5,1,3,7,4],expected: [1,3,4,5,7]),
-                         (data: [2,1,3,7,4],expected: [1,2,3,4,7]),
-                         (data: [7,4],expected: [4,7])]
+        
         //act
         //assert
         for testCase in testCases {
@@ -24,18 +27,66 @@ final class SortingTests: XCTestCase {
         }
     }
     
+    func testPerformaceBubbleSort() {
+        let sorting = Sorting()
+        let testCases = [(data: [5,1,3,7,4],expected: [1,3,4,5,7])]
+                        
+        measure {
+            for testCase in testCases {
+                let actual = sorting.bubbleSort(testCase.data)
+                XCTAssertEqual(actual,testCase.expected)
+            }
+        }
+    }
+    
     func testMergeSortWithManyShuffledArraysOfIntegersReturnsThemInAscendingOrder() {
         //arange
         let sorting = Sorting()
-        let testCases = [(data: [5,1,3,7,4],expected: [1,3,4,5,7]),
-                         (data: [2,1,3,7,4],expected: [1,2,3,4,7]),
-                         (data: [7,4],expected: [4,7])]
+        
         //act
         
         //assert
         for testCase in testCases {
-            let actual = sorting.bubbleSort(testCase.data)
+            let actual = sorting.mergeSort(testCase.data)
             XCTAssertEqual(actual,testCase.expected)
+        }
+    }
+    
+    func testPerformaceMergeSort() {
+        let sorting = Sorting()
+        let testCases = [(data: [5,1,3,7,4],expected: [1,3,4,5,7])]
+                        
+        measure {
+            for testCase in testCases {
+                let actual = sorting.mergeSort(testCase.data)
+                XCTAssertEqual(actual,testCase.expected)
+            }
+        }
+    }
+    
+    func testQuickSortWithManyShuffledArraysOfIntegersReturnsThemInAscendingOrder(){
+        //arange
+        let sorting = Sorting()
+        
+        //act
+        
+        //assert
+        for testCase in testCases {
+            let actual = sorting.quickSort(testCase.data)
+            XCTAssertEqual(actual,testCase.expected)
+            
+        }
+    }
+    
+    func testPerformaceQuickSort() {
+        let sorting = Sorting()
+        let testCases = [(data: [5,1,3,7,4],expected: [1,3,4,5,7])]
+                        
+        measure {
+            for testCase in testCases {
+                let actual = sorting.quickSort(testCase.data)
+                XCTAssertEqual(actual,testCase.expected)
+            }
         }
     }
 }
