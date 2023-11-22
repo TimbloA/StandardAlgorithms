7e4aa29707f78aedf401d54ee9723295a02a5b7e
Adi Timblo <timblo.a@etoncollege.org.uk>
2023-11-22 18:46:08 +0000

    Finished Coding in Main to add full functionality, if I have time will come back and add demonstrations for everything. Had to relearn how to access attributes in classes, but looking through Tamagotchi code helped!

--- StandardAlgorithms/main.swift
+++ StandardAlgorithms/main.swift
@@ -18,8 +18,72 @@ print("""
       Searching:
       5. Linear Search
       6. Binary Search
+      7. Break
       """)
-var userInput = readLine()
-print(userInput!)
 
-
+while true {
+    print("Enter Value: ")
+    var userInput = readLine()
+    
+    if userInput == "1" {
+        print("""
+    
+    \(bubbleSortInfo.name)
+    \(bubbleSortInfo.infoText)
+    Worst Time Complexity: \(bubbleSortInfo.worstBigONotation)
+    Best Time Complexity: \(bubbleSortInfo.bestBigONotation)
+    Average Time Complexity: \(bubbleSortInfo.avBigONotation)
+    """)
+    }
+    else if userInput == "2" {
+        print("""
+    \(mergeSortInfo.name)
+    \(mergeSortInfo.infoText)
+    Worst Time Complexity: \(mergeSortInfo.worstBigONotation)
+    Best Time Complexity: \(mergeSortInfo.bestBigONotation)
+    Average Time Complexity: \(mergeSortInfo.avBigONotation)
+    """)
+    }
+    else if userInput == "3" {
+        print("""
+    \(quickSortInfo.name)
+    \(quickSortInfo.infoText)
+    Worst Time Complexity: \(quickSortInfo.worstBigONotation)
+    Best Time Complexity: \(quickSortInfo.bestBigONotation)
+    Average Time Complexity: \(quickSortInfo.avBigONotation)
+    """)
+    }
+    else if userInput == "4" {
+        print("""
+    \(insertionSortInfo.name)
+    \(insertionSortInfo.infoText)
+    Worst Time Complexity: \(insertionSortInfo.worstBigONotation)
+    Best Time Complexity: \(insertionSortInfo.bestBigONotation)
+    Average Time Complexity: \(insertionSortInfo.avBigONotation)
+    """)
+    }
+    else if userInput == "5" {
+        print("""
+    \(linearSearchInfo.name)
+    \(linearSearchInfo.infoText)
+    Worst Time Complexity: \(linearSearchInfo.worstBigONotation)
+    Best Time Complexity: \(linearSearchInfo.bestBigONotation)
+    Average Time Complexity: \(linearSearchInfo.avBigONotation)
+    """)
+    }
+    else if userInput == "6" {
+        print("""
+    \(binarySearchInfo.name)
+    \(binarySearchInfo.infoText)
+    Worst Time Complexity: \(binarySearchInfo.worstBigONotation)
+    Best Time Complexity: \(binarySearchInfo.bestBigONotation)
+    Average Time Complexity: \(binarySearchInfo.avBigONotation)
+    """)
+    }
+    else if userInput == "7" {
+        break
+    }
+    else {
+        print("Invalid Input")
+    }
+}
